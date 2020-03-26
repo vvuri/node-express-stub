3e2d0b88024945c8997a1f171ab207c4a8873e2c
vvuri <vvuri1978@gmail.com>
2020-03-26 14:01:15 +0300

    Add express routing

--- src/fs_server.js
+++ src/fs_server.js
@@ -1,4 +1,3 @@
-import * as http from 'http';
 import * as fs from 'fs';
 import * as mime from 'mime-types';
 
@@ -15,64 +14,63 @@ console.log = function (d) {
     logFile.write(util.format(d) + '\n');
 };
 
+import express from 'express';
+const app = express();
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        let data;
+const rootDir = (req, res, next, subdir = '') => {
+    let data;
 
-        getDir(dirname, 'utf-8')
-            .then(files => {
-                data = `<h2>List Files in <i>${dirname}</i>:</h2>`;
-                data += `<ul> ${files} </ul>`;
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'text/html');
-                console.log('Return:' + data);
-                res.end(data);
-            })
-            .catch(error => {
-                console.log(error);
-                res.statusCode = 404;
-                res.end('Dir Not Found');
-            });
-    }
-    else {
-        console.log('url file:' + req.url);
-        fs.readFile(dirname + req.url, (err, data) => {
-            if (err) {
-                switch (err['code']) {
-                    case 'EPERM':
-                        res.statusCode = 423;
-                        res.end('Access Denied');
-                        break;
-                    case 'EISDIR':
-                        res.statusCode = 406;
-                        res.end('SubDirectory not allow');
-                        break;
-                    case 'ENOENT':
-                        res.statusCode = 404;
-                        res.end('File not Found');
-                        break;
-                    default:
-                        res.statusCode = 403;
-                        res.end('Forbidden');
-                        break;
-                }
-                console.log(JSON.stringify(err));
-                return;
-            }
-            console.log(`ContentType: ${mime.lookup(req.url)}`);
-            res.setHeader('Content-Type', mime.lookup(req.url));
-            res.writeHead(200);
+    console.log(`Dir: ${subdir}`);
+    getDir(dirname.concat(subdir), subdir, 'utf-8')
+        .then(files => {
+            data = `<h2>List Files in <i>${dirname}${subdir}</i>:</h2>`;
+            data += `<ul> ${files} </ul>`;
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            console.log('Return:' + data);
             res.end(data);
+        })
+        .catch(error => {
+            console.log(error);
+            res.statusCode = 404;
+            res.end('Dir Not Found');
         });
-    }
-});
+};
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+const filesDir = (req, res) => {
+    console.log(`url file:  ${req.url},  ${req.method},  ${req.params.id},  ${req.originalUrl}`);
+    fs.readFile(dirname + req.url, (err, data) => {
+        if (err) {
+            switch (err['code']) {
+                case 'EPERM':
+                    res.statusCode = 423;
+                    res.end('Access Denied');
+                    break;
+                case 'EISDIR':
+                    rootDir(req, res, null, req.url);
+                    break;
+                case 'ENOENT':
+                    res.statusCode = 404;
+                    res.end('File not Found');
+                    break;
+                default:
+                    res.statusCode = 403;
+                    res.end('Forbidden');
+                    break;
+            }
+            console.log(JSON.stringify(err));
+            return;
+        }
+        console.log(`ContentType: ${mime.lookup(req.url)}`);
+        res.setHeader('Content-Type', mime.lookup(req.url));
+        req.acceptsCharsets('utf-8');
+        res.writeHead(200);
+        res.end(data);
+    });
+};
 
-function getDir (folder, enconding) {
+function getDir (folder, subdir, enconding) {
+    console.log(`Folder: ${folder}, ${subdir}`);
     return new Promise((resolve, reject) => {
         fs.readdir(folder, enconding, (err, items) => {
             if (err)
@@ -81,9 +79,18 @@ function getDir (folder, enconding) {
                 let data = '';
 
                 for (const item of items)
-                    data += `<li> ${item} (<A href="http://${hostname}:${port}/${item}">open</A>) </li>`;
+                    data += `<li> ${item} (<A href="http://${hostname}:${port}${subdir}/${item}">open</A>) </li>`;
                 resolve(data);
             }
         });
     });
 }
+
+app.get('/', rootDir);
+app.get('/:id', filesDir);
+app.get('/elements/:id', filesDir);
+app.get('/elements/subelements/:id', filesDir);
+
+app.listen(port, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});

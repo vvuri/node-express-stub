f651f7897c37921a8d17428a1d27948afc2e7cf5
Yuri Vinogradov <vvuri1978@gmail.com>
2020-04-02 13:27:52 +0300

    Fix in fs_config

--- src/fs_config.js
+++ src/fs_config.js
@@ -7,21 +7,19 @@ debug(`HOST: ${process.env.HOST}\nPORT: ${process.env.PORT}\nROOT_DIR: ${process
 
 let error = false;
 
-if (process.env.HOST == undefined) {
+if (typeof process.env.HOST === 'undefined')
     error = 'HOST not allow';
-}
 
-if (process.env.PORT == undefined) {
+if (typeof process.env.PORT === 'undefined')
     error = 'PORT not allow';
-}
 
-if (process.env.ROOT_DIR == undefined) {
+if (typeof process.env.ROOT_DIR === 'undefined')
     error = 'ROOT_DIR not allow';
-}
 
 if (error) {
     console.log(`\x1b[31mError: ${error}\x1b[0m\nPlease rename .env_simle to .env and restart server\n`);
     console.log(`HOST: ${process.env.HOST}\nPORT: ${process.env.PORT}\nROOT_DIR: ${process.env.ROOT_DIR}`);
+    // eslint-disable-next-line
     process.exit();
 }
 

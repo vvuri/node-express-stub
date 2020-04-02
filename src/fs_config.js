fa85c9afe47d61ec05907b3001e605809528d187
Yuri Vinogradov <vvuri1978@gmail.com>
2020-04-02 21:12:57 +0300

    Return config.json

--- src/fs_config.js
+++ src/fs_config.js
@@ -2,20 +2,41 @@ import dotenv from 'dotenv';
 import chalk from 'chalk';
 
 import debug from './fs_logger';
+import config from '../config.json';
+
+const { hostname, port, dirname } = config;
 
 dotenv.config();
-debug(`HOST: ${process.env.HOST}\nPORT: ${process.env.PORT}\nROOT_DIR: ${process.env.ROOT_DIR}`);
+debug(`Env::   HOST: ${process.env.HOST}  PORT: ${process.env.PORT}  ROOT_DIR: ${process.env.ROOT_DIR}`);
+debug(`Json::  HOST: ${hostname}  PORT: ${port}  ROOT_DIR: ${dirname}`);
 
+export let HOST = process.env.HOST;
+export let PORT = process.env.PORT;
+export let ROOT_DIR = process.env.ROOT_DIR;
+
+// Add from config, if environment is null
 const errors = [];
 
-if (typeof process.env.HOST === 'undefined')
-    errors.push(`Environment variable ${chalk.green('HOST')} not found`);
+if (typeof process.env.HOST === 'undefined') {
+    if (typeof hostname === 'undefined')
+        errors.push(`Environment variable ${chalk.green('HOST')} not found`);
+    else
+        HOST = hostname;
+}
 
-if (typeof process.env.PORT === 'undefined')
-    errors.push(`Environment variable ${chalk.green('PORT')} not found`);
+if (typeof process.env.PORT === 'undefined') {
+    if (typeof port === 'undefined')
+        errors.push(`Environment variable ${chalk.green('PORT')} not found`);
+    else
+        PORT = port;
+}
 
-if (typeof process.env.ROOT_DIR === 'undefined')
-    errors.push(`Environment variable ${chalk.green('ROOT_DIR')} not found`);
+if (typeof process.env.ROOT_DIR === 'undefined') {
+    if (typeof dirname === 'undefined')
+        errors.push(`Environment variable ${chalk.green('ROOT_DIR')} not found`);
+    else
+        ROOT_DIR = dirname;
+}
 
 if (errors.length) {
     for (const error of errors)
@@ -26,7 +47,3 @@ if (errors.length) {
     // eslint-disable-next-line
     process.exit();
 }
-
-export const HOST = process.env.HOST;
-export const PORT = process.env.PORT;
-export const ROOT_DIR = process.env.ROOT_DIR;

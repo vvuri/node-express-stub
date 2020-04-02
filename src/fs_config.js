21d055f8b2122d3cd197d17a632cc499935dc8ac
Yuri Vinogradov <vvuri1978@gmail.com>
2020-04-02 13:00:27 +0300

    Add negative scenario environment

--- src/fs_config.js
+++ src/fs_config.js
@@ -5,6 +5,26 @@ import debug from './fs_logger';
 dotenv.config();
 debug(`HOST: ${process.env.HOST}\nPORT: ${process.env.PORT}\nROOT_DIR: ${process.env.ROOT_DIR}`);
 
+let error = false;
+
+if (process.env.HOST == undefined) {
+    error = 'HOST not allow';
+}
+
+if (process.env.PORT == undefined) {
+    error = 'PORT not allow';
+}
+
+if (process.env.ROOT_DIR == undefined) {
+    error = 'ROOT_DIR not allow';
+}
+
+if (error) {
+    console.log(`\x1b[31mError: ${error}\x1b[0m\nPlease rename .env_simle to .env and restart server\n`);
+    console.log(`HOST: ${process.env.HOST}\nPORT: ${process.env.PORT}\nROOT_DIR: ${process.env.ROOT_DIR}`);
+    process.exit();
+}
+
 export const HOST = process.env.HOST;
 export const PORT = process.env.PORT;
 export const ROOT_DIR = process.env.ROOT_DIR;

5c194223220d03a14a3097019a95fd2d9ab0bd08
Yuri Vinogradov <vvuri1978@gmail.com>
2020-05-06 15:03:53 +0300

    Modify fs promisify

--- src/fs_helper.js
+++ src/fs_helper.js
@@ -1,45 +1,39 @@
 import fs from 'fs';
 import debug from './fs_logger';
-//mport util from 'util';
+import util from 'util';
+
+const fsStat = util.promisify(fs.stat);
+const fsReaddir = util.promisify(fs.readdir);
 
 const statDir = fileName => {
-    return new Promise((resolve, reject) => {
-        fs.stat(fileName, (err, stat) => {
-            if (err) {
-                debug(err, '_statDir.error');
-                reject(err);
-            }
-            else
-                resolve(stat);
+    return fsStat(fileName)
+        .catch( err => {
+            debug(err, 'statDir.error');
+            return err;
         });
-    });
 };
 
 export const getDir = ( folder, subdir, enconding ) => {
-    return new Promise((resolve, reject) => {
-        fs.readdir(folder, enconding, (err, items) => {
-            if (err) {
-                debug(err, '_getDir.error');
-                reject(err);
-            }
-            else
-                resolve(items);
+    return fsReaddir(folder, enconding)
+        .catch( err => {
+            debug(err, 'getDir.error');
+            return err;
         });
-    });
 };
 
 export const getListSubDirectories = (rootDir, dirPath, subdir = '') => {
     return getDir(rootDir.concat(subdir), subdir, 'utf-8')
         .then(files => {
             return Promise.all(files.map(fileName => {
-                // eslint-disable-next-line
-                return statDir(`${rootDir}${subdir}/${fileName}`).then( stat => {
-                    if (stat.isDirectory()) {
-                        debug(`${subdir}/${fileName}`, 'getListSubDirectories');
-                        dirPath.push(`${subdir}/${fileName}`);
-                        return getListSubDirectories(rootDir, dirPath, `${subdir}/${fileName}`);
-                    }
-                });
+                return statDir(`${rootDir}${subdir}/${fileName}`)
+                    .then( stat => {
+                        if (stat.isDirectory()) {
+                            debug(`${subdir}/${fileName}`, 'getListSubDirectories');
+                            dirPath.push(`${subdir}/${fileName}`);
+                            return getListSubDirectories(rootDir, dirPath, `${subdir}/${fileName}`);
+                        }
+                        return null;
+                    });
             }));
         });
 };

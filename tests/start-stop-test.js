c414fe583d2c74c08bcb4aade58f5b21f4b6f791
Yuri Vinogradov <vvuri1978@gmail.com>
2020-05-21 15:54:15 +0300

    Change start-stop

--- tests/start-stop-test.js
+++ tests/start-stop-test.js
@@ -1,35 +1,47 @@
 import assert from 'assert';
-import { createRequester, getClearConfig, parseLiList, testConfig, testConfigSecond } from './helper';
+import { createRequester, getClearConfig, parseLiList, stopSrv, testConfig } from './helper';
+import fs from 'fs';
+import proxyquire from 'proxyquire';
 import StaticServer from '../src/fs_server';
 
 let srv;
 let requester;
-let requesterSecond;
+
+proxyquire.noPreserveCache();
 
 describe('Start/stop API', () => {
-    let result;
-
     before(async () => {
         getClearConfig();
         requester = createRequester();
         srv = new StaticServer(testConfig);
     });
 
-    after(async () => {
-        await srv.stop(); //stopServer(result.server);
+    describe('Negative: server running tests:', () => {
+        it('Error when deleted dir after start server', async () => {
+            const newdir = testConfig.rootDir.concat('/elements/newdir');
+
+            fs.mkdirSync(newdir, '0744');
+            await srv.start();
+            fs.rmdirSync(newdir, { recursive: true });
+
+            const res = await requester.get('/elementas/newdir');
+
+            assert.equal(res.status, 404);
+            assert.equal(srv.isRunning, true);
+            await stopSrv(srv);
+        });
     });
 
     describe('Positive: server running tests:', () => {
         beforeEach(async () => {
-            result = await srv.start();
+            await srv.start();
         });
 
         afterEach(async () => {
-            await srv.stop();
+            await stopSrv(srv);
         });
 
         it('Server starting and response by HTTP', async () => {
-            assert.equal(result, null);
             const res = await requester.get('/');
 
             assert.equal(res.status, 200);
@@ -55,104 +67,177 @@ describe('Start/stop API', () => {
 
     describe('Server stopping test', () => {
         beforeEach(async () => {
-            result = await srv.start();
-            result = await srv.stop();
+            await srv.start();
         });
 
         it('Stopping the server does not result in an error', async () => {
-            assert.equal(result, null, 'The returned parameters error = null');
+            await srv.stop();
+            assert.equal(srv.isRunning, false);
         });
 
         it('Stopping a stopped server results in an error', async () => {
-            result = await srv.stop(); // stopServer(result.server);
-
-            assert.equal(result.message, 'Server is not running.');
+            await srv.stop();
+            await assert.rejects(
+                async () => await srv.stop(),
+                {
+                    name:    'Error',
+                    message: 'Server is not running.'
+                });
         });
     });
 
     describe('Negative server running tests:', () => {
         it('Don`t run Server if incorrect port', async () => {
             srv.port = 100500;
-            result = await srv.start();
-
-            assert.equal(result, 'Error: options.port should be >= 0 and < 65536. Received 100500.');
+            try {
+                await srv.start();
+            }
+            catch (error) {
+                assert.equal(error, 'Error: options.port should be >= 0 and < 65536. Received 100500.');
+            }
+            finally {
+                await stopSrv(srv);
+            }
         });
 
-        const runs = [
-            { it: null, options: `Cannot read object 'server'` },
-            { it: 'foo', options: `Error: Server NOT stopped!` },
-            { it: { foo: 'bar' }, options: `Error: Server NOT stopped!` }
-        ];
+        it('Don`t run Server on nonexistent directory', async () => {
+            srv.rootDir = 'private';
+            try {
+                await srv.start();
+            }
+            catch (error) {
+                assert.equal(error.substr(0, 33), 'ENOENT: no such file or directory');
+            }
+        });
 
-        runs.forEach(run => {
-            it(`Don't stop Server with unacceptable parameter: ${run.it}`, async () => {
-                srv.server = run.it;
-                const resultError = await srv.stop(); // stopServer(run.it);
+        it('Return error message in statDir() if we can not read information about a file', async () => {
+            const { statDir } = proxyquire('../dist/fs_helper', { 'fs': { stat: () => {
+                        throw new Error('Run mock statDir()');
+                    } } });
+            const error = await statDir(testConfig.rootDir);
 
-                assert.equal(resultError.message, run.options);//`Error: Cannot read object 'server'`);
-            });
+            assert(error.error, 'Error getting information about a file:public: Run mock statDir()');
+        });
+
+        // TODO: Modify the test when calling srv.start. Use mock-require
+        it('Interrupt execution if directory tree traversal error', async () => {
+            try {
+                const { getListSubDirectories } = proxyquire('../dist/fs_helper', {
+                    'fs': {
+                        'stat': ( path, callback ) => {
+                            callback(null, {
+                                isDirectory: () => {
+                                    throw new Error(`Mock error`);
+                                }
+                            });
+                        }
+                    }
+                });
+
+                await getListSubDirectories(testConfig.rootDir);
+            }
+            catch (err) {
+                assert.equal(err, `Mock error`);
+            }
         });
     });
 });
 
 describe(`Running two servers on different ports and with different paths`, () => {
-    let result;
+    let requesterSecond;
 
     before(async () => {
+        const testConfigSecond = { host: testConfig.host, port: '9999', rootDir: 'public/elements' };
+
         getClearConfig();
         requester = createRequester();
         requesterSecond = createRequester(testConfigSecond.host, testConfigSecond.port);
 
-        srv = { first: null, second: null };
-
-        srv.first = new StaticServer(testConfig);
-        srv.second = new StaticServer(testConfigSecond);
+        srv = {
+            first:  new StaticServer(testConfig),
+            second: new StaticServer(testConfigSecond)
+        };
     });
 
-    beforeEach(`Started without errors`, async () => {
-        result = await srv.first.start();
-        assert.equal(result, null);
+    describe(`Started two servers without errors`, () => {
+        after(async () => {
+            await stopSrv(srv.first);
+            await stopSrv(srv.second);
+        });
 
-        result = await srv.second.start();
-        assert.equal(result, null);
+        it(`Started without errors`, async () => {
+            await srv.first.start();
+            assert.equal(srv.first.isRunning, true);
+
+            await srv.second.start();
+            assert.equal(srv.second.isRunning, true);
+        });
+
+        it(`Each server responds to a request`, async () => {
+            const resFirst = await requester.get('/');
+            const resSecond = await requesterSecond.get('/');
+
+            assert.equal(resFirst.status, 200);
+            assert.equal(resSecond.status, 200);
+        });
+
+        it(`The displayed lists of the subdirectory of one and the main directory of another are identical`, async () => {
+            const resFirst = await requester.get('/elements');
+            const resSecond = await requesterSecond.get('/');
+
+            const docFirst = parseLiList(resFirst.text);
+            const docSecond = parseLiList(resSecond.text);
+
+            assert.equal(JSON.stringify(docFirst), JSON.stringify(docSecond));
+        });
     });
 
-    afterEach(async () => {
-        await srv.first.stop();
-        await srv.second.stop();
+    describe(`Stopping one of servers`, () => {
+        beforeEach(`Started without errors`, async () => {
+            await srv.first.start();
+            await srv.second.start();
+        });
+
+        afterEach(async () => {
+            await stopSrv(srv.first);
+            await stopSrv(srv.second);
+        });
+
+        it(`Stopping one don't stop the other server`, async () => {
+            await srv.second.stop();
+            assert.equal(srv.second.isRunning, false);
+            const result = await requester.get('/');
+
+            assert.equal(result.status, 200);
+        });
+
+        it(`Restarting the second server on the same port`, async () => {
+            await srv.second.stop();
+            await srv.second.start();
+            assert.equal(srv.second.isRunning, true);
+        });
+    });
+});
+
+describe('Negative two server running tests:', () => {
+    before(async () => {
+        srv = {
+            first:  new StaticServer(testConfig),
+            second: new StaticServer(testConfig)
+        };
+        await srv.first.start();
     });
 
-    it(`Each server responds to a request`, async () => {
-        const resFirst = await requester.get('/');
-        const resSecond = await requesterSecond.get('/');
-
-        assert.equal(resFirst.status, 200);
-        assert.equal(resSecond.status, 200);
+    after(async () => {
+        await stopSrv(srv.first);
     });
 
-    it(`The displayed lists of the subdirectory of one and the main directory of another are identical`, async () => {
-        const resFirst = await requester.get('/elements');
-        const resSecond = await requesterSecond.get('/');
-
-        const docFirst = parseLiList(resFirst.text);
-        const docSecond = parseLiList(resSecond.text);
-
-        assert.equal(JSON.stringify(docFirst), JSON.stringify(docSecond));
-    });
-
-    it(`Restarting the second server on the same port`, async () => {
-        result = await srv.second.stop();
-        assert.equal(result, null, 'Server stopped without Error');
-
-        result = await srv.second.start();
-        assert.equal(result, null, 'Server Restarted without Error');
-    });
-
-    it(`Stopping one don't stop the other server`, async () => {
-        result = await srv.second.stop();
-        assert.equal(result, null, 'Server stopped without Error');
-
-        result = await requester.get('/');
-        assert.equal(result.status, 200);
+    it('Don`t run second Server on the same port', async () => {
+        try {
+            await srv.second.start();
+        }
+        catch (error) {
+            assert.equal(error, `Error: listen EADDRINUSE: address already in use :::${testConfig.port}`);
+        }
     });
 });
